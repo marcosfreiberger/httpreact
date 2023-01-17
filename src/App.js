1e3c578fc17ccdac21eaf4b91ea6eb1d4a6ac569
Marcos Freiberger <marcos.freiberger@bradesco.com.br>
2023-01-17 14:24:15 -0300

    Resgatando dados da API

--- src/App.js
+++ src/App.js
@@ -1,9 +1,35 @@
 import './App.css';
 
+import { useState, useEffect } from 'react';
+
+const url = 'http://localhost:3000/products'
+
 function App() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const response = await fetch(url);
+
+      const data = await response.json();
+
+      setProducts(data);
+    };
+
+    fetchData();
+  }, []);
+
+
   return (
     <div className="App">
       <h1>Lista de Produtos</h1>
+      <ul>
+        {products.map(product => (
+          <li key={product.id}>
+            {product.name} - R$ {product.price}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
